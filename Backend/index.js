f4cd9eebb25edc317cdc2e668f877bfed46153bb
Shivam Jha <108573467+ShivamKumarJha2004@users.noreply.github.com>
2024-10-19 20:05:58 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -109,7 +109,7 @@ app.post("/upload", upload.single('product'), (req, res) => {
     res.json({
         success: 1,
         // Ensure the correct hosted URL is used for the image
-        image_url: `https://stylemart-ecom-backend.onrender.com/images/${req.file.filename}`
+        image_url: `https://stylemartbackend.onrender.com/images/${req.file.filename}`
     });
 });
 
